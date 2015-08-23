244a6aa24c8ca0e00fde102d9286c25655fef017
mrgelmir <sanderdecleer@gmail.com>
2015-08-23 21:14:17 +0200

    added start menu and started on a finish tile

---
+++ UNITY/Assets/Code/Grid/EndTile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTile : GridTile
+{
+	public override void OnEnterTile(GridItem actor)
+	{
+		base.OnEnterTile(actor);
+
+		PlayerMover player = actor as PlayerMover;
+		if(player != null)
+		{
+			//victory: later on send a message to the GameManager
+			Application.LoadLevel(0);
+		}
+	}
+	
+}
---
+++ UNITY/Assets/Code/UI/LoadLevelButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Button))]
+public class LoadLevelButton : MonoBehaviour 
+{
+	[SerializeField]
+	private Button button;
+
+	[SerializeField]
+	private int LevelToLoad = 0;
+
+	protected void OnEnable()
+	{
+
+		if (button == null)
+			button = GetComponent<Button>();
+
+		if(button != null)
+			button.onClick.AddListener(OnClick);
+	}
+
+	protected void OnDisable()
+	{
+		if (button == null)
+			button = GetComponent<Button>();
+
+		if (button != null)
+			button.onClick.RemoveListener(OnClick);
+	}
+
+	private void OnClick()
+	{
+		Application.LoadLevel(LevelToLoad);
+	}
+}

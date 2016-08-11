43a89abc15a5133c0c2c795bad0e107f96c1a609
Sander Decleer <sanderdecleer@gmail.com>
2016-08-11 23:17:11 +0200

    Added Substance material testing

---
+++ UNITY/Assets/Test_sd/SubstanceMech/MechRandomizer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MechRandomizer : MonoBehaviour {
+
+	public float rotateSpeed = 1f;
+	private ProceduralMaterial mat;
+	
+	protected void Start()
+	{
+		mat = GetComponent<Renderer>().material as ProceduralMaterial;
+
+		StartCoroutine(RandomizeRoutine());
+	}
+
+	private IEnumerator RandomizeRoutine()
+	{
+		for(;;)
+		{
+			Randomize();
+			yield return new WaitForSeconds(Random.Range(3f, 5f));
+		}
+	}
+
+	public void Randomize()
+	{
+		mat.SetProceduralFloat("Dirt", Random.Range(0f, 1f));
+		mat.SetProceduralFloat("Hue", Random.Range(0f, 1f));
+		mat.SetProceduralFloat("Saturation", Random.Range(0f, 1f));
+
+		mat.SetProceduralColor("LensColor", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+
+		mat.RebuildTextures();
+	}
+
+	void Update ()
+	{
+		// Temp rotate model
+		transform.Rotate(Vector3.forward, Time.deltaTime * rotateSpeed);
+	}
+}
